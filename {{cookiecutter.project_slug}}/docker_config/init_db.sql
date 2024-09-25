dd0d85717f3a918f705c79a37a0c6fa2ea945579
Artem Suvorov <artem.suvorov@gmail.com>
2024-09-25 14:59:50 -0400

    Initial Commit...

--- {{cookiecutter.project_slug}}/docker_config/init_db.sql
+++ {{cookiecutter.project_slug}}/docker_config/init_db.sql
@@ -1,2 +1,9 @@
-CREATE DATABASE IF NOT EXISTS {{cookiecutter.project_slug}};
-CREATE USER     IF NOT EXISTS root IDENTIFIED BY 'root';
+{{ if cookiecutter.db_engine=="MySQL" }}
+CREATE DATABASE IF NOT EXISTS {{ cookiecutter.db_name }};
+CREATE USER     IF NOT EXISTS {{ db_username}} IDENTIFIED BY '{{ db_password}}';
+{{ elif cookiecutter.db_engine=="PostgreSQL" }}
+SELECT 'CREATE DATABASE {{ cookiecutter.db_name }}'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '{{ cookiecutter.db_name }}')\gexec
+
+CREATE USER {{ cookiecutter.db_username }} WITH PASSWORD '{{ cookiecutter.db_password }}' CREATEDB;
+{{ endif }}
